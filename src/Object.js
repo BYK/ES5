ecbb9c8080994aeb7501a28b7123e0c5be0a550a
Burak Yiğit Kaya <madbyk@gmail.com>
2011-12-11 10:20:36 +0000

    Fixed Object.keys to recover if the object iself has a key named 'hasOwnProperty' and modified the unit test to test this behavior. Added test for Object.getPrototypeOf. (progresses #2)

--- src/Object.js
+++ src/Object.js
@@ -10,7 +10,7 @@ if (!Object.keys)
 /**
  * Returns an array of all enumerable properties found directly upon a given object, in the same order as that provided by a for (var p in o) loop.
  * Taken from https://developer.mozilla.org/En/Core_JavaScript_1.5_Reference/Global_Objects/Object/Keys
- * 
+ *
  * @param {Object} obj The object whose enumerable own properties are to be returned.
  * @return {Array} All enumerable properties found directly upon the given object.
  */
@@ -18,7 +18,7 @@ Object.keys = function(obj)
 {
 	var result = [];
 	for(var name in obj)
-		if (obj.hasOwnProperty(name))
+		if (Object.prototype.hasOwnProperty.appl(obj, name))
 			result.push(name);
 
 	return result;
@@ -27,7 +27,7 @@ Object.keys = function(obj)
 if (!Object.getPrototypeOf)
 /**
  * Returns the prototype of the specified object.
- * 
+ *
  * @param {Object} obj The object whose prototype will be accessed
  * @return {Object} The prototype object of the provided object
  */
--- test/unit/Object.js
+++ test/unit/Object.js
@@ -7,9 +7,18 @@ test('Basic requirements', function () {
 });
 
 test('keys()', function () {
-    expect(2);
+    expect(3);
     var array = ['a', 'b', 'c'];
     var obj = { 0: 'a', 1: 'b', 2: 'c' };
+    var hasOwnProp = {'a': 1, 'hasOwnProperty': 2};
     equal(Object.keys(array).toString(), '0,1,2', 'Returns an array whose elements are strings corresponding to the enumerable properties found directly upon object.');
     equal(Object.keys(obj).toString(), '0,1,2', 'Array like object.');
+    equal(Object.keys(hasOwnProp).toString(), 'a,hasOwnProperty', 'Does not get confused by a key named "hasOwnProperty".');
+});
+
+test('getPrototypeOf', function () {
+    expect(1);
+    var MyClass = function MyClass(){};
+    var myObject = new MyClass();
+    equal(Object.getPrototypeOf(myObject), MyClass.prototype, "Properly gives the prototype of the constructor.");
 });
